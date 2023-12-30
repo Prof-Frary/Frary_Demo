c6c0742727afea2a07b7142bd3068ddd5b6ab823
Prof-Frary <bfrary@qcc.cuny.edu>
2023-12-30 18:54:31 -0500

    Added Text property of Form and added a label

--- Frary_Demo/Form1.Designer.cs
+++ Frary_Demo/Form1.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(282, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(212, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Prof Frary's Project";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Your Name Here";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
