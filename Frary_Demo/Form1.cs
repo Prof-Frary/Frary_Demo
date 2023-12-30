ac50e5734995582369f11c9fcf0d59b6a73c4763
Prof-Frary <bfrary@qcc.cuny.edu>
2023-12-30 18:38:52 -0500

    Add project files.

---
+++ Frary_Demo/Form1.cs
@@ -0,0 +1,15 @@
+namespace Frary_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
